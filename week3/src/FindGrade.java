0320fb7a4a88df3a57e11246cacaecfe8fb3a9bc
helinbaydur <medinehelinbaydur@posta.mu.edu.tr>
2025-03-07 11:58:25 +0300

    Update FindGrade.java

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -10,6 +10,32 @@ public class FindGrade{
         // 70 - 79 -> C
         // 60 - 69 -> D
         // 0 - 59 -> F
+        
+        if (args.length >= 1) {
+
+
+            int score = Integer.parseInt(args[0]);
+            if (score >= 90) {
+                System.out.println("Grade: A");
+            } else if (score >= 80 && score < 89) {
+                System.out.println("Grade: B");
+
+            } else if (score >= 70 && score < 79) {
+                System.out.println("Grade: C");
+
+            } else if (score >= 60 && score < 69) {
+                System.out.println("Grade: D");
+
+            } else if (score >= 0 && score < 59) {
+                System.out.println("Grade: F");
+            } else {
+                System.out.println("Invalid Grade");
+
+            }
+        }else{
+            System.out.println("Please provide a score");
+        }
+    }   }
 
 
 
